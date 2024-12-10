d0277c9f90b95795392fb109c73f5c2ab8e6e295
Samet Candan <c._samet@hotmail.com>
2024-12-10 07:03:56 +0300

    homework 7

--- patika.sql
+++ patika.sql
@@ -119,3 +119,23 @@
 --SELECT COUNT(DISTINCT replacement_cost) FROM film
 --WHERE length>150;
 
+--HOMEWORK 7
+
+--1-film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+--SELECT rating, COUNT(*)  FROM film
+--GROUP BY rating;
+
+--2-film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+--SELECT replacement_cost, COUNT(*)  FROM film
+--GROUP BY replacement_cost
+--HAVING COUNT(*) > 50;
+
+--3-customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+--SELECT store_id, COUNT(*) FROM customer
+--GROUP BY store_id;
+
+--4-city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+--SELECT country_id, COUNT(*) AS ct FROM city
+--GROUP BY country_id
+--ORDER BY ct DESC 
+--LIMIT 1;
